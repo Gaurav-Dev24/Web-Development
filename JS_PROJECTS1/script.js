e47fc83f9676381823894457cf304a2565e80924
GAURAV KUMAR DEV <66454708+Gaurav-Dev24@users.noreply.github.com>
2022-06-26 13:25:07 +0530

    Update script.js

--- JS_PROJECTS1/script.js
+++ JS_PROJECTS1/script.js
@@ -1,1 +1,38 @@
+// const red = document.querySelector(".red");
+// const cyan = document.querySelector(".cyan");
+// const violet = document.querySelector(".violet");
+// const orange = document.querySelector(".orange");
+// const pink = document.querySelector(".pink");
 
+// const centre = document.querySelector(".centre");
+
+// console.log(window.getComputedStyle(red).backgroundColor);
+
+// const getBGcolor = (selectedElement) => {
+//     return window.getComputedStyle(selectedElement).backgroundColor
+// };
+
+// console.log(getBGcolor(pink));
+
+
+// Event listener
+
+const red = document.querySelector(".red");
+const cyan = document.querySelector(".cyan");
+const violet = document.querySelector(".violet");
+const orange = document.querySelector(".orange");
+const pink = document.querySelector(".pink");
+
+const centre = document.querySelector(".centre");
+
+// console.log(window.getComputedStyle(red).backgroundColor);
+
+const getBGcolor = (selectedElement) => {
+    return window.getComputedStyle(selectedElement).backgroundColor
+};
+
+var pinkElementColor = getBGcolor(pink);
+pink.addEventListener("mouseenter", () => {
+    centre.style.background = pinkElementColor;
+
+});
